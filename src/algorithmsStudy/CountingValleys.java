eb2625154f667a69139e8e3943888ab5c42df32e
BinSin <binyong318@gmail.com>
2017-09-19 16:07:33 +0900

    CountingValleys

---
+++ src/algorithmsStudy/CountingValleys.java
@@ -0,0 +1,38 @@
+package algorithmsStudy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class CountingValleys {
+
+	public static int countValley(char[] c) {
+		int length = c.length;
+		int seaLevel = 0;
+		int vallyNumber = 0;
+		
+		for(int i=0; i<length; i++) {
+			if(c[i] == 'U') {
+				seaLevel++;
+				if(seaLevel == 0)
+					vallyNumber++;
+			}
+			else {
+				seaLevel--;
+			}
+		}
+		return vallyNumber;
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		String str = br.readLine();
+		char[] c = new char[n];
+		for(int i=0; i<n; i++) {
+			c[i] = str.charAt(i);
+		}
+		
+		System.out.println(countValley(c));
+	}
+}
