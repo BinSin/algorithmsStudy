900712c768d5ff6331ca8cb5326be50d21c5e261
BinSin <binyong318@gmail.com>
2017-10-28 23:27:54 +0900

    Strange Counter

---
+++ src/algorithmsStudy4/StrangeCounter.java
@@ -0,0 +1,29 @@
+/*
+ * Author : BinSin
+ * https://www.hackerrank.com/challenges/strange-code
+ */
+
+package algorithmsStudy4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class StrangeCounter {
+
+	public static long conuterValues(long t) {
+		long value = 3;
+		while(t > value) {
+			t = t - value;
+			value *= 2;
+		}
+		return value - t + 1;
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		long t = Long.parseLong(br.readLine());
+		System.out.println(conuterValues(t));
+	}
+}
