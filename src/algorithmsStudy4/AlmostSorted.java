53220925304fc2ddf1350673ae7f3141154993c4
BinSin <binyong318@gmail.com>
2018-07-18 17:28:17 +0900

    Amost Sorted

--- src/algorithmsStudy4/AlmostSorted.java
+++ src/algorithmsStudy4/AlmostSorted.java
@@ -12,110 +12,79 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class AlmostSorted {
-
-	public static boolean checkSort(ArrayList<Integer> A) {
-		@SuppressWarnings("unchecked")
-		ArrayList<Integer> B = (ArrayList<Integer>)A.clone();
-		Collections.sort(B);
-		if(A.equals(B)) {
-			return true;
-		}
-		else {
-			return false;
-		}
-	}
-	
-	public static void swap(ArrayList<Integer> A, int a, int b) {
-		int atmp = A.get(a);
-		int btmp = A.get(b);
-		A.set(a, btmp);
-		A.set(b, atmp);
-	}
-	
-	public static boolean checkReverse(ArrayList<Integer> A, int a, int b) {
-		ArrayList<Integer> B = new ArrayList<Integer>();
-		
-		for(int i=a; i<=b; i++) {
-			B.add(A.get(i));
-		}
-		
-		@SuppressWarnings("unchecked")
-		ArrayList<Integer> C = (ArrayList<Integer>)B.clone();
-		Collections.sort(C);
-		Collections.reverse(C);
-		if(B.equals(C)) {
-			return true;
-		}
-		else {
-			return false;
-		}
-	}
-	
-	public static int[] compare(ArrayList<Integer> A, int n) {
-		int count = 0;
-		int t = 0;
-		int[] num = new int[2];
-		for(int i=0; i<n-1; i++) {
-			if(A.get(i+1) < A.get(i)) {
-				count++;
-				t = i+1;
-				num[0] = A.get(i+1);
-				break;
-			}
-		}
-		
-		if(count == 0) return null;
-		
-		for(int i=t; i<n-1; i++) {
-			if(A.get(i+1) < A.get(i)) {
-				count++;
-				t = i+1;
-				num[1] = A.get(i+1);
-				break;
-			}
-		}
-		
-		for(int i=t; i<n-1; i++) {
-			if(A.get(i+1) < A.get(i)) {
-				return null;
-			}
-		}
-		
-		return num;
-	}
 	
 	public static void almostSorted(ArrayList<Integer> A, int n) {
-		if(checkSort(A)) {
-			System.out.println("yes");
+		if(n == 2) {
+			if(A.get(1) < A.get(0)) {
+				System.out.println("yes");
+				System.out.println("swap 1 2");
+			}
+			else {
+				System.out.println("yes");
+			}
 			return;
 		}
 		
-		int[] num = compare(A, n);
-		for(int i=0; i<n-1; i++) {
-			for(int j=i+1; j<n; j++) {
-				@SuppressWarnings("unchecked")
-				ArrayList<Integer> tmp = (ArrayList<Integer>)A.clone();
-				swap(tmp, i, j);
-				if(checkSort(tmp)) {
-					System.out.println("yes");
-					System.out.println("swap " + (i+1) + " " + (j+1));
-					return;
+		int count = 0;
+		int e1=1, e2=2;
+		@SuppressWarnings("unchecked")
+		ArrayList<Integer> B = (ArrayList<Integer>)A.clone();
+		Collections.sort(B);
+		for(int i=0; i<n; i++) {
+			if(B.get(i) != A.get(i)) {
+				count++;
+				if(count == 1) {
+					e1 = i+1;
+				}
+				else if(count == 2) {
+					e2 = i+1;
 				}
 			}
 		}
 		
-		int t = 1;
-		for(int i=n-1; i>2; i--) {
-			for(int j=0; j<t; j++) {
-				if(checkReverse(A, j, i)) {
+		if(count == 0) {
+			System.out.println("yes");
+			return;
+		}
+		else if(count == 2) {
+			System.out.println("yes");
+			System.out.println("swap " + e1 + " " + e2);
+			return;
+		}
+
+		int gap = 0, rev = 0;
+		int start = 1, end = 2;
+		for(int i=0; i<n-1; i++) {
+			if(A.get(i) >= A.get(i+1)) {
+				gap++;
+				if(gap == 1)
+					start = i+1;
+				else if(gap > 1)
+					end = i+2;
+			}
+			else {
+				if(gap >= 2) {
 					System.out.println("yes");
-					System.out.println("reverse " + (j+1) + " " + (i+1));
+					System.out.println("reverse " + start + " " + end);
 					return;
 				}
+				if(gap == 1) {
+					rev++;
+				}
+				if(rev > 1) {
+					System.out.println("no");
+					return;
+				}
+				gap = 0;
 			}
-			t++;
 		}
-		System.out.println("no");
+		if(gap >= 2) {
+			System.out.println("yes");
+			System.out.println("reverse " + start + " " + n);
+		}
+		else {
+			System.out.println("no");
+		}
 	}
 	
 	public static void main(String[] args) throws IOException {
