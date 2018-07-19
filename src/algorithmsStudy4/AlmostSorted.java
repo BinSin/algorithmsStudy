98ee3794fe6e824738dc33429eeb9b026a474a43
BinSin <binyong318@gmail.com>
2018-07-19 17:14:38 +0900

    Almost Sorted

--- src/algorithmsStudy4/AlmostSorted.java
+++ src/algorithmsStudy4/AlmostSorted.java
@@ -10,95 +10,52 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.StringTokenizer;
 
 public class AlmostSorted {
 	
-	public static void almostSorted(ArrayList<Integer> A, int n) {
-		if(n == 2) {
-			if(A.get(1) < A.get(0)) {
-				System.out.println("yes");
-				System.out.println("swap 1 2");
-			}
-			else {
-				System.out.println("yes");
-			}
-			return;
+	public static void reverse(int[] A, int start, int end) {
+		while(start < end) {
+			swap(A, start, end);
+			start++;
+			end--;
 		}
-		
-		int count = 0;
-		int e1=1, e2=2;
-		@SuppressWarnings("unchecked")
-		ArrayList<Integer> B = (ArrayList<Integer>)A.clone();
-		Collections.sort(B);
+	}
+	
+	public static void swap(int[] A, int start, int end) {
+		int temp;
+		temp = A[start];
+		A[start] = A[end];
+		A[end] = temp;
+	}
+	
+	public static boolean isSort(int[] A, int n) {
 		for(int i=0; i<n; i++) {
-			if(B.get(i) != A.get(i)) {
-				count++;
-				if(count == 1) {
-					e1 = i+1;
-				}
-				else if(count == 2) {
-					e2 = i+1;
-				}
+			if(A[i] > A[i+1]) {
+				return false;
 			}
 		}
+		return true;
+	}
+	
+	public static void almostSorted(int[] A, int n) {
 		
-		if(count == 0) {
-			System.out.println("yes");
-			return;
-		}
-		else if(count == 2) {
-			System.out.println("yes");
-			System.out.println("swap " + e1 + " " + e2);
-			return;
-		}
-
-		int gap = 0, rev = 0;
-		int start = 1, end = 2;
-		for(int i=0; i<n-1; i++) {
-			if(A.get(i) >= A.get(i+1)) {
-				gap++;
-				if(gap == 1)
-					start = i+1;
-				else if(gap > 1)
-					end = i+2;
-			}
-			else {
-				if(gap >= 2) {
-					System.out.println("yes");
-					System.out.println("reverse " + start + " " + end);
-					return;
-				}
-				if(gap == 1) {
-					rev++;
-				}
-				if(rev > 1) {
-					System.out.println("no");
-					return;
-				}
-				gap = 0;
-			}
-		}
-		if(gap >= 2) {
-			System.out.println("yes");
-			System.out.println("reverse " + start + " " + n);
-		}
-		else {
-			System.out.println("no");
-		}
+		
+		
 	}
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
 		
-		String str = br.readLine();
-		String[] s = str.split("\\s+");
-		
-		ArrayList<Integer> A = new ArrayList<Integer>(n);
-		for(int i=0; i<n; i++) {
-			A.add(Integer.parseInt(s[i]));
+		int i =0;
+		int[] A = new int[n];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		while(st.hasMoreTokens()) {
+			int t = Integer.parseInt(st.nextToken());
+			A[i++] = t;
 		}
-
+		
 		almostSorted(A, n);
 		
 		br.close();
