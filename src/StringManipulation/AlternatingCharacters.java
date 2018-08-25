cf4d8fdde9caba42f12f1eec336a03804bc9984a
BinSin <binyong318@gmail.com>
2018-08-25 16:03:50 +0900

    Alternating Characters

---
+++ src/StringManipulation/AlternatingCharacters.java
@@ -0,0 +1,28 @@
+package StringManipulation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class AlternatingCharacters {
+
+	private static int alternatingCharacters(String s) {
+		int count = 0;
+		for(int i=0; i<s.length()-1; i++) {
+			if(Math.abs(s.charAt(i) - s.charAt(i+1)) == 0)
+				count++;
+		}
+		
+		return count;
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int q = Integer.parseInt(br.readLine());
+		
+		while(q-->0) {
+			String s = br.readLine();
+			System.out.println(alternatingCharacters(s));
+		}
+	}
+}
