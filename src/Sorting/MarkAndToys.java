91fbb1fdb19dc16bf3c5cad453d25765c4cc1173
BinSin <binyong318@gmail.com>
2018-08-26 20:42:44 +0900

    Mark and Toys

--- src/Sorting/MarkAndToys.java
+++ src/Sorting/MarkAndToys.java
@@ -1,3 +1,8 @@
+/*
+ * Author: BinSin
+ * https://www.hackerrank.com/challenges/mark-and-toys/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=sorting
+ */
+
 package Sorting;
 
 import java.io.BufferedReader;
